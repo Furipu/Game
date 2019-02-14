f54c74aaab8faf7ec279e9277f877ac49f2fe360
Furipu <vandamme.philippe@gmail.com>
2019-02-14 22:39:22 +0100

    Add time interval

--- server/modules/World.js
+++ server/modules/World.js
@@ -6,6 +6,7 @@ module.exports = class World {
     this.allPlayersMap = new Map();
     this.allFightsMap = new Map();
     this.lockedPositionsMap = new Map();
+    this.timeOutFights();
   }
 
   getAllPlayers() {
@@ -14,26 +15,30 @@ module.exports = class World {
   }
 
   addPlayer(name) {
-
     //Nieuwe speler wordt toegevoegd. Naam wordt doorgegeven vanuit server.js functie app.get('/setPlayer/:name')
     //Positie wordt bepaald obv functie initPosition. Deze geeft een object met x en y positie terug
     const playerPosition = this.initPosition();
-    //Nieuwe speler maken obv de klasse Player - x en y positie worden ook opgeslagen 
+    //Nieuwe speler maken obv de klasse Player - x en y positie worden ook opgeslagen
     //in de spelersklasse om gemakkelijk op te kunnen zoeken
-    const player = new Player(name,playerPosition.xPos,playerPosition.yPos); 
-    this.allPlayersMap.set(this.makeKey(playerPosition.xPos,playerPosition.yPos), player);
-    return returnAll();
-  }
-
-  returnAll(){
+    const player = new Player(name, playerPosition.xPos, playerPosition.yPos);
+    this.allPlayersMap.set(
+      this.makeKey(playerPosition.xPos, playerPosition.yPos),
+      player
+    );
     return {
-      'players': this.allPlayersMap,
-      'fights': this.allFightsMap
+      players: this.allPlayersMap,
+      fights: this.allFightsMap
     };
   }
-  
+
+  returnAll() {
+    return {
+      players: this.allPlayersMap,
+      fights: this.allFightsMap
+    };
+  }
+
   initPosition() {
-    
     let positionExists = true;
     let xPos;
     let yPos;
@@ -43,10 +48,10 @@ module.exports = class World {
     let xArr = [];
     let yArr = [];
 
-    this.allPlayersMap.forEach(function (val) {
+    this.allPlayersMap.forEach(function(val) {
       xArr.push(val.xPos);
       yArr.push(val.yPos);
-    })
+    });
 
     let xBase = this.median(xArr);
     let yBase = this.median(yArr);
@@ -55,7 +60,6 @@ module.exports = class World {
     //let yBase = this.average(yArr);
 
     while (positionExists) {
-  
       xPos = ~~this.getRndInteger(xBase, xBase + 100);
       yPos = ~~this.getRndInteger(yBase, yBase + 100);
 
@@ -66,83 +70,91 @@ module.exports = class World {
       } else {
         positionExists = false;
       }
-
     }
 
     //Object teruggeven met x en y positie als resultaat van de functie
-    return {'xPos':xPos, 'yPos':yPos};
+    return { xPos: xPos, yPos: yPos };
   }
 
-  movePlayer(xFrom,yFrom,xTo,yTo) {
-    
+  movePlayer(xFrom, yFrom, xTo, yTo) {
     let posFrom = this.makeKey(xFrom, yFrom);
     let posTo = this.makeKey(xTo, yTo);
 
-    if(!this.checkPositionLocked(xTo,yTo)){
-      
-      this.lockPosition(xTo,yTo);
-  
+    if (!this.checkPositionLocked(xTo, yTo)) {
+      this.lockPosition(xTo, yTo);
+
       if (!this.allPlayersMap.has(posFrom)) {
         //Player bestaat niet
         this.unlockPosition(xTo, yTo);
-        return 'no player';
+        return "no player";
       }
-  
+
       if (this.allPlayersMap.has(posTo)) {
         //vechten
-        let fightId = posFrom + '|' + posTo;
+        let fightId = posFrom + "|" + posTo;
         let fight = {
-          'id':fightId,
-          'timestamp':Date.now(), // timestamp om timeout te bepalen als spelers niet tijdig een wapen kiezen - speler die niet tijdig reageert = dood
-          'playerFrom': this.allPlayersMap.get(posFrom),
-          'playerTo': this.allPlayersMap.get(posTo)
+          id: fightId,
+          timestamp: Date.now(), // timestamp om timeout te bepalen als spelers niet tijdig een wapen kiezen - speler die niet tijdig reageert = dood
+          playerFrom: this.allPlayersMap.get(posFrom),
+          playerTo: this.allPlayersMap.get(posTo)
         };
         return this.allFightsMap.set(fightId, fight);
-
-
       } else {
         let player = this.allPlayersMap.get(posFrom);
         player.xPos = xTo;
         player.yPos = yTo;
         this.allPlayersMap.set(posTo, player);
         this.allPlayersMap.delete(posFrom);
-  
-        this.unlockPosition(xTo,yTo);
 
-        return returnAll();
+        this.unlockPosition(xTo, yTo);
 
+        return {
+          players: this.allPlayersMap,
+          fights: this.allFightsMap
+        };
       }
-    }else{
+    } else {
       return {
-        'locked': {
-          'from':posFrom,
-          'to': posTo
+        locked: {
+          from: posFrom,
+          to: posTo
         }
       };
     }
-
-
   }
 
-  timeOutFights(){
+  timeOutFights() {
+    let timeToCheck = new Date();
+    setInterval(() => {
+      timeToCheck = Date.now() - 10000;
+      for (const fight of Object.values(this.allFightsMap)) {
+        if (fight.timestamp < timeToCheck) {
+          if (fight.playerFrom.weapon === "")
+            this.allPlayersMap.delete(fight.playerFrom);
+          if (fight.playerTo.weapon === "")
+            this.allPlayersMap.delete(fight.playerTo);
+          return this.allPlayersMap;
+        }
+      }
+      console.log(`${new Date(Date.now())} - ${timeToCheck}`);
+    }, 10000);
+
     //initieren in constructor
     //iteratieve check of fight langer dan 10 seconden bezig is
     //indien timeout -> speler die niet gereageerd heeft, dus zonder wapen, sterft (kan allebei zijn)
     //sterven =  verwijderen uit array
     //volledige allPlayersmap en fightMap teruggeven aan front
-    this.unlockPosition(xTo, yTo);
-
+    //this.unlockPosition(xTo, yTo);
   }
 
-  fight(fight){
-
+  fight(fight) {
     let actualFight = this.allFightsMap.get(fight.id);
     let actualPlayerId = fight.player.id;
 
     switch (actualPlayerId) {
       case actualFight.playerFrom.id:
         actualFight.playerFrom.weapon = fight.player.weapon;
-        this.allFightsMap.set(fight.id,actualFight);
+        this.allFightsMap.set(fight.id, actualFight);
         break;
       case actualFight.playerTo.id:
         actualFight.playerTo.weapon = fight.player.weapon;
@@ -153,21 +165,14 @@ module.exports = class World {
         break;
     }
 
-    if (actualFight.playerFrom.weapon || actualFight.playerTo.weapon){
-      return 'waitforopponent';
+    if (actualFight.playerFrom.weapon || actualFight.playerTo.weapon) {
+      return "waitforopponent";
     }
-
-    
-
 
     //indien wel - > logica blad steen schaar + verliezer verwijderen uit array en winnaar op to positie
     //volledige allPlayersmap en fightMap teruggeven aan front
-    
+
     this.unlockPosition(xTo, yTo);
-
-
-
-
   }
 
   makeKey(x, y) {
@@ -175,17 +180,18 @@ module.exports = class World {
   }
 
   median(numbers) {
-
     let median = 0;
     let count = numbers.length;
 
-    numbers.sort(function (a, b) {
-      return a - b
+    numbers.sort(function(a, b) {
+      return a - b;
     });
 
-    if (count % 2 === 0) { // even
+    if (count % 2 === 0) {
+      // even
       median = numbers[count / 2];
-    } else { // oneven
+    } else {
+      // oneven
       median = numbers[(count - 1) / 2];
     }
 
@@ -194,42 +200,38 @@ module.exports = class World {
     }
 
     return median;
-
   }
 
   average(numbers) {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    let average;
 
-     const reducer = (accumulator, currentValue) => accumulator + currentValue;
-     let average;
+    if (numbers.length !== 0) {
+      average = numbers.reduce(reducer) / numbers.length;
+    } else {
+      average = 1;
+    }
 
-     if (numbers.length !== 0) {
-       average = numbers.reduce(reducer) / numbers.length;
-     } else {
-       average = 1;
-     }
-
-     return average;
-
-   }
-
+    return average;
+  }
 
   getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  lockPosition(xPos, yPos){
+  lockPosition(xPos, yPos) {
     let pos = this.makeKey(xPos, yPos);
     this.lockedPositionsMap.set(pos, true);
   }
 
-  unlockPosition(xPos, yPos){
+  unlockPosition(xPos, yPos) {
     let pos = this.makeKey(xPos, yPos);
     this.lockedPositionsMap.delete(pos);
   }
 
-  checkPositionLocked(xPos, yPos){
+  checkPositionLocked(xPos, yPos) {
     let pos = this.makeKey(xPos, yPos);
-    if(this.lockedPositionsMap.has(pos)){
+    if (this.lockedPositionsMap.has(pos)) {
       console.log(true);
       return true;
     }
